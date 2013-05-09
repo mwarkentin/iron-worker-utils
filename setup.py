2ea1e5a861047a6e4629027fbb6e80e86eafd8a0
Michael Warkentin <mwarkentin@gmail.com>
2013-05-09 10:45:31 -0400

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='iron-worker-utils',
-    version='0.1.0',
+    version='0.1.1',
     packages=['iron_worker_utils', ],
     license=open('LICENSE').read(),
     description='A collection of utilities for writing IronWorker tasks.',
